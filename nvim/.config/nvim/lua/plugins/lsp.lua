774a73f9993ef1474277410fd5299a8e5a54f265
wragdan <17775924+Wragdan@users.noreply.github.com>
2024-07-10 02:01:29 -0600

    chore: simplify lsp configuration

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,21 @@
 return {
     {
         'VonHeikemen/lsp-zero.nvim',
+        dependencies = {
+            { 'neovim/nvim-lspconfig' },
+            {
+                'williamboman/mason.nvim',
+                lazy = false,
+                config = true,
+            },
+            { 'L3MON4D3/LuaSnip' },
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'rafamadriz/friendly-snippets' },
+        },
         branch = 'v3.x',
         lazy = true,
         config = function()
@@ -8,7 +23,49 @@ return {
 
             lsp_zero.on_attach(function(_, bufnr)
                 lsp_zero.default_keymaps({ buffer = bufnr })
+
+                lsp_zero.default_keymaps({ buffer = bufnr, preserve_mappings = true })
+
+                local opts = { buffer = bufnr }
+                local bind = vim.keymap.set
+
+                bind('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+                bind('n', '<leader>f',
+                    function() vim.lsp.buf.format({ async = false, timeout_ms = 10000 }) end, opts)
+                bind('n', ';d', function() vim.diagnostic.open_float() end, opts)
+                bind('n', '<leader>rn', function() vim.lsp.buf.rename() end, opts)
+                bind('n', '<leader>ca', function() vim.lsp.buf.code_action() end, opts)
+                bind('n', '<leader>rr', function() vim.lsp.buf.references() end, opts)
+                bind('n', ']d', function() vim.diagnostic.goto_next() end, opts)
+                bind('n', '[d', function() vim.diagnostic.goto_prev() end, opts)
+                bind('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
             end)
+
+            local cmp = require('cmp')
+            local cmp_action = require('lsp-zero').cmp_action()
+
+            require('luasnip.loaders.from_vscode').lazy_load()
+
+            cmp.setup({
+                sources = {
+                    { name = 'path' },
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip', keyword_length = 2 },
+                    { name = 'buffer',  keyword_length = 3 },
+                },
+                formatting = lsp_zero.cmp_format(),
+                mapping = cmp.mapping.preset.insert({
+                    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<Tab>'] = cmp_action.luasnip_supertab(),
+                    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+                }),
+                snippet = {
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body)
+                    end,
+                },
+            })
 
             lsp_zero.format_on_save({
                 format_opts = {
@@ -23,57 +80,34 @@ return {
                 }
             })
 
+            lsp_zero.configure('rust_analyzer', {
+                settings = {
+                    ["rust-analyzer"] = {
+                        checkOnSave = {
+                            command = "clippy",
+                            extraArgs = { "--target-dir=./target/check" },
+                        },
+                    },
+                }
+            })
+
             vim.diagnostic.config({
                 virtual_text = true
             })
         end,
         init = function()
             -- Disable automatic setup, we are doing it manually
-            vim.g.lsp_zero_extend_cmp = 0
-            vim.g.lsp_zero_extend_lspconfig = 0
-            vim.g.lsp_zero_ui_float_border = 0
+            --vim.g.lsp_zero_extend_cmp = 0
+            --vim.g.lsp_zero_extend_lspconfig = 0
+            --vim.g.lsp_zero_ui_float_border = 0
         end,
-    },
-    {
-        'williamboman/mason.nvim',
-        lazy = false,
-        config = true,
-    },
-    {
-        'hrsh7th/nvim-cmp',
-        event = 'InsertEnter',
-        dependencies = {
-            { 'L3MON4D3/LuaSnip' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-        },
-        config = function()
-            -- Here is where you configure the autocompletion settings.
-            local lsp_zero = require('lsp-zero')
-            lsp_zero.extend_cmp()
-
-            -- And you can configure cmp even more, if you want to.
-            local cmp = require('cmp')
-            local cmp_action = require('lsp-zero').cmp_action()
-            local cmp_format = require('lsp-zero').cmp_format({ details = true })
-
-            require('luasnip.loaders.from_vscode').lazy_load()
-
-            cmp.setup({
-                sources = {
-                    { name = 'nvim_lsp' },
-                    { name = 'luasnip' },
-                },
-                formatting = lsp_zero.cmp_format(),
-                mapping = cmp.mapping.preset.insert({
-                    ['<CR>'] = cmp.mapping.confirm({ select = false }),
-                    ['<C-Space>'] = cmp.mapping.complete(),
-                })
-            })
-        end
     },
     {
         'williamboman/mason-lspconfig.nvim',
         lazy = false,
+        dependencies = {
+            { 'williamboman/mason.nvim' }
+        },
         config = function()
             local lsp_zero = require('lsp-zero')
             local lsp_config = require('lspconfig')
@@ -119,42 +153,4 @@ return {
             })
         end
     },
-
-    -- LSP
-    {
-        'neovim/nvim-lspconfig',
-        config = function()
-            local lsp_zero = require('lsp-zero')
-            lsp_zero.extend_lspconfig()
-
-            lsp_zero.configure('rust_analyzer', {
-                settings = {
-                    ["rust-analyzer"] = {
-                        checkOnSave = {
-                            command = "clippy",
-                            extraArgs = { "--target-dir=./target/check" },
-                        },
-                    },
-                }
-            })
-
-            lsp_zero.on_attach(function(_, bufnr)
-                lsp_zero.default_keymaps({ buffer = bufnr, preserve_mappings = true })
-
-                local opts = { buffer = bufnr }
-                local bind = vim.keymap.set
-
-                bind('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-                bind('n', '<leader>f',
-                    function() vim.lsp.buf.format({ async = false, timeout_ms = 10000 }) end, opts)
-                bind('n', ';d', function() vim.diagnostic.open_float() end, opts)
-                bind('n', '<leader>rn', function() vim.lsp.buf.rename() end, opts)
-                bind('n', '<leader>ca', function() vim.lsp.buf.code_action() end, opts)
-                bind('n', '<leader>rr', function() vim.lsp.buf.references() end, opts)
-                bind('n', ']d', function() vim.diagnostic.goto_next() end, opts)
-                bind('n', '[d', function() vim.diagnostic.goto_prev() end, opts)
-                bind('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
-            end)
-        end
-    }
 }
